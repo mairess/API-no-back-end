02fd75eafc1ab275a4e03d2332506bf11a2cadca
Maires Souza <trabalho.maires@gmail.com>
2023-04-23 12:07:56 -0300

    add POST

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const app = express()
 
 app.listen("3000")
 
+//middleware
+app.use(express.json())
 
-app.route('/').get((req, res) => res.send('Hello!'))
-app.route('/sobre').get((req, res) => res.send('Hello sobre!'))
-app.route('/contato').get((req, res) => res.send('Hello contato!'))
+
+// app.route('/').get((req, res) => res.send('Hello!'))
+// app.route('/sobre').get((req, res) => res.send('Hello sobre!'))
+// app.route('/contato').get((req, res) => res.send('Hello contato!'))
+
+app.route('/').post((req, res) => res.send(req.body))
