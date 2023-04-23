81d0dbc1ce65e496b889521a310d9e38261c516c
Maires Souza <trabalho.maires@gmail.com>
2023-04-23 11:53:44 -0300

    update index.js

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const express = require('express')
 const app = express()
 
 app.listen("3000")
+
+
+app.route('/').get((req, res) => res.send('Hello!'))
+app.route('/sobre').get((req, res) => res.send('Hello sobre!'))
+app.route('/contato').get((req, res) => res.send('Hello contato!'))
