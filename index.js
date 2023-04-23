a03f1ec125f08cd9f41840d6a9269a666dc9397f
Maires Souza <trabalho.maires@gmail.com>
2023-04-23 12:30:23 -0300

    add PUT

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ app.listen("3000")
 //middleware
 app.use(express.json())
 
+let author = 'Maires'
+
+
+
+app.route('/').get((req, res) => res.send(author))
+
+
+
 
 // app.route('/').get((req, res) => res.send('Hello!'))
 // app.route('/sobre').get((req, res) => res.send('Hello sobre!'))
 // app.route('/contato').get((req, res) => res.send('Hello contato!'))
 
-app.route('/').post((req, res) => res.send(req.body))
+// app.route('/').post((req, res) => res.send(req.body))
+
+app.route('/').put((req, res) => {
+    author = req.body.author
+    res.send(author)
+})
+
+
